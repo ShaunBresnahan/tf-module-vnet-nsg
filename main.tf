2b3bf3b595ecfd63ec4fd92bc8f238fe62f397a3
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2021-07-06 20:56:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 locals {
   base_cidr_block = var.address
 }
@@ -37,22 +38,23 @@ resource "azurerm_subnet" "spokesubnet" {
   virtual_network_name = azurerm_virtual_network.spokevnet.name
   address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets[count.index].number)]
   service_endpoints    = var.service_endpoints
-
   dynamic "delegation" {
-    for_each = try(var.subnets[count.index].delegation, null) == null ? [] : [1]
-    content {
-      name = "delegation"
+      for_each = try(var.subnets[count.index].delegation, null) == null ? [] : [1]
+      content {
+        name = "delegation"
 
-      service_delegation {
-        name    = var.subnets[count.index].delegation.name
-        actions = try(var.subnets[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
-      }
+        service_delegation {
+          name    = var.subnets[count.index].delegation.name
+          actions = try(var.subnets[count.index].delegation.actions, ["Microsoft.Network/virtualNetworks/subnets/action"])
+       }
     }
   }
-}
+  lifecycle { ignore_changes = [enforce_private_link_endpoint_network_policies, delegation] }
+} 
 
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
+  provider                  = azurerm.src
   count                     = length(var.subnets)
   subnet_id                 = azurerm_subnet.spokesubnet[count.index].id
   network_security_group_id = azurerm_network_security_group.nsg.id
-  }
+}
