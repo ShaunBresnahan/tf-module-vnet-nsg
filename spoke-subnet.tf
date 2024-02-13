0123d4ea70c22dec2511c1e23aa4e925e96ec970
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2024-02-13 20:19:19 +0000

    Create spoke-subnet.tf

---
+++ spoke-subnet.tf
@@ -0,0 +1,17 @@
+resource "azurerm_subnet" "spokesubnet" {
+  count                = length(var.subnets)
+  name                 = var.subnets[count.index].name
+  provider             = azurerm.src
+  resource_group_name  = var.resource_group.name
+  virtual_network_name = azurerm_virtual_network.spokevnet.name
+  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, var.subnets[count.index].number)]
+  service_endpoints    = var.service_endpoints
+  lifecycle { ignore_changes = [enforce_private_link_endpoint_network_policies, delegation] }
+} 
+
+resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
+  provider                  = azurerm.src
+  count                     = length(var.subnets)
+  subnet_id                 = azurerm_subnet.spokesubnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
