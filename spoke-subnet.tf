aa356561fec5928e0e8c0f8742af9cf2559c8711
Shaun Bresnahan <shedman28@googlemail.com>
2025-08-07 22:22:17 +0100

    testy

--- spoke-subnet.tf
+++ spoke-subnet.tf
@@ -1,7 +1,5 @@
-
-# Subnets WITH delegation
-resource "azurerm_subnet" "spokesubnet_with_delegation" {
-  for_each             = { for s in var.subnets : s.name => s if s.delegation_name != null }
+resource "azurerm_subnet" "spokesubnet" {
+  for_each             = { for s in var.subnets : s.name => s }
   name                 = each.value.name
   provider             = azurerm.src
   resource_group_name  = var.resource_group.name
@@ -9,33 +7,67 @@ resource "azurerm_subnet" "spokesubnet_with_delegation" {
   address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, each.value.number)]
   service_endpoints    = var.service_endpoints
 
-  delegation {
-    name = "delegation"
-    service_delegation {
-      name    = each.value.delegation_name
-      actions = each.value.delegation_actions
+  dynamic "delegation" {
+    for_each = each.value.delegation_name != null ? [1] : []
+    content {
+      name = "delegation"
+      service_delegation {
+        name    = each.value.delegation_name
+        actions = each.value.delegation_actions
+      }
     }
   }
 }
 
-# Subnets WITHOUT delegation
-resource "azurerm_subnet" "spokesubnet_without_delegation" {
-  for_each             = { for s in var.subnets : s.name => s if s.delegation_name == null }
-  name                 = each.value.name
-  provider             = azurerm.src
-  resource_group_name  = var.resource_group.name
-  virtual_network_name = azurerm_virtual_network.spokevnet.name
-  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, each.value.number)]
-  service_endpoints    = var.service_endpoints
-}
-
-# NSG association for all subnets
 resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
   provider                  = azurerm.src
-  for_each                  = merge(azurerm_subnet.spokesubnet_with_delegation, azurerm_subnet.spokesubnet_without_delegation)
+  for_each                  = azurerm_subnet.spokesubnet
   subnet_id                 = each.value.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+
+
+
+
+
+## Subnets WITH delegation
+#resource "azurerm_subnet" "spokesubnet_with_delegation" {
+#  for_each             = { for s in var.subnets : s.name => s if s.delegation_name != null }
+#  name                 = each.value.name
+#  provider             = azurerm.src
+#  resource_group_name  = var.resource_group.name
+#  virtual_network_name = azurerm_virtual_network.spokevnet.name
+#  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, each.value.number)]
+#  service_endpoints    = var.service_endpoints
+#
+#  delegation {
+#    name = "delegation"
+#    service_delegation {
+#      name    = each.value.delegation_name
+#      actions = each.value.delegation_actions
+#    }
+#  }
+#}
+#
+## Subnets WITHOUT delegation
+#resource "azurerm_subnet" "spokesubnet_without_delegation" {
+#  for_each             = { for s in var.subnets : s.name => s if s.delegation_name == null }
+#  name                 = each.value.name
+#  provider             = azurerm.src
+#  resource_group_name  = var.resource_group.name
+#  virtual_network_name = azurerm_virtual_network.spokevnet.name
+#  address_prefixes     = [cidrsubnet(local.base_cidr_block, var.newbits, each.value.number)]
+#  service_endpoints    = var.service_endpoints
+#}
+#
+## NSG association for all subnets
+#resource "azurerm_subnet_network_security_group_association" "spokesubnetnsg" {
+#  provider                  = azurerm.src
+#  for_each                  = merge(azurerm_subnet.spokesubnet_with_delegation, azurerm_subnet.spokesubnet_without_delegation)
+#  subnet_id                 = each.value.id
+#  network_security_group_id = azurerm_network_security_group.nsg.id
+#}
 
 
 
