31aa4070f148dfcd34bc01917093eb23685c70cc
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2025-08-19 15:36:14 +0100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnets" {
-  description = "Map of subnet resources created"
-  value       = azurerm_subnet.spokesubnet
-}
-
-output "subnet_ids" {
-  description = "Map of subnet IDs keyed by name"
-  value       = { for k, s in azurerm_subnet.spokesubnet : k => s.id }
-}
